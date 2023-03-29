07f67c353f5c6afc053806ad73bf3e4ee7a72c6e
Nevin Zachariah (Intern) <nevin.zachariah@bytestrone.com>
2023-03-29 23:23:57 +0530

    intial commit

---
+++ Patterns/src/pattern/pattern6.java
@@ -0,0 +1,29 @@
+package pattern;
+public class pattern6 {
+	public static void main(String[] args) {
+		int i,j;
+		for(i=0;i<5;i++) {
+			for(j=0;j<5;j++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+}
+//
+//}
+//package pattern;
+//public class pattern3 {
+//public static void main(String[] args) {
+//	int i,j,row;
+//	row=5;
+//	for(i=0;i<=row;i++) {
+//		for(j=0;j<row-i+1;j++){
+//			System.out.print("*");
+//			
+//		}
+//		System.out.println(" ");
+//	}
+//}
+//
+//}
